c3d3bf601873a8391575370c380dfd07b725fe66
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-02-06 12:24:12 -0600

    testing php

---
+++ src/main/java/phpTest/TalkingToDB.php
@@ -0,0 +1,43 @@
+<?php
+	$dbhost="localhost";
+    $dbname="customers";
+    $dbuser="root";
+    $dbpassword="";
+
+    $con=@mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+
+$query= "select * from customerInformation";
+$cust_query= mysqli_query($con,$query);
+?>
+
+<html>
+<head>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+</head>
+<body>
+<button id="hideButton">Click me!</button>
+
+<table id="tableHide">
+    <tr>
+        <th>Info for this human</th>
+        <?php
+			while($customer = mysqli_fetch_assoc($cust_query)) {
+		?>
+        
+        <td><?=$customer['fullName']?></td>
+        <td><?=$customer['email']?></td>
+        <td><?=$customer['phone']?></td>
+
+    </tr>
+        <?php
+            }?>
+</table>
+
+<script>
+    $("#hideButton").click(function () {
+        $("#tableHide").toggle();
+    });
+</script>
+
+</body>
+</html>
---
+++ src/main/java/soufTest.java
@@ -0,0 +1,6 @@
+public class soufTest {
+    public static void main(String[] args) {
+        Double num = 3.45;
+
+    }
+}
